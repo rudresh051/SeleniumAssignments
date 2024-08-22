0e924957a0ee478f51f2d2611aea2217da4508cd
Rudreshwar Jha <jharudreshwar17@gmail.com>
2024-08-22 23:11:01 +0530

    Product listing page sorting test cases added

--- src/test/java/tests/ProductListingPageTestCases.java
+++ src/test/java/tests/ProductListingPageTestCases.java
@@ -79,6 +79,11 @@ public class ProductListingPageTestCases extends BaseTest {
 			sel3.selectByIndex(4);
 			Thread.sleep(5000);
 			
+			// Best Sellers
+			Select sel5 = new Select(ProductListingPageVariable.sortingDropdown);
+			sel3.selectByIndex(5);
+			Thread.sleep(5000);
+			
 			
 		} catch (Exception e) {
 			// TODO: handle exception
